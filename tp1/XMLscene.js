859c836ddb9abe0bca1e72766ca2879fe905f7e2
Henrique Nunes <up201906852@fe.up.pt>
2022-10-09 14:24:33 +0100

    Change material with m key

--- tp1/XMLscene.js
+++ tp1/XMLscene.js
@@ -141,6 +141,16 @@ export class XMLscene extends CGFscene {
     }
 
 
+    update(t){
+        if (this.gui.isKeyPressed("KeyM")) {
+            for (let key in this.graph.components) {
+                this.graph.components[key].nextMaterial();
+            }
+        }
+
+    }
+
+
     /**
      * Displays the scene.
      */
