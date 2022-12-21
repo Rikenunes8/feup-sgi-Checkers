0e799346da128912b19e1e4540eba11a70ac079d
Rui Alves <up201905853@edu.fe.up.pt>
2022-12-21 15:27:06 +0000

    Integrated Text with buttons. Text is independent of screen (change this later)

--- tp3/components/MyButton.js
+++ tp3/components/MyButton.js
@@ -1,13 +1,18 @@
 import { Pickable } from "../checkers/Pickable.js";
+import { writeText } from "../checkers/utils.js";
 import { MyRectangle } from "./MyRectangle.js";
 
 export class MyButton extends Pickable {
-    constructor(scene, id, p1, p2, pickable, pickId, onPick) {
+    constructor(scene, id, p1, p2, pickable, pickId, onPick, text, translate, rotate, scale) {
         super(pickId, pickable);
         this.scene = scene;
         this.id = id;
         this.pickId = pickId;
         this.button = new MyRectangle(scene, id, p1[0], p2[0], p1[1], p2[1]);
+        this.text = text;
+        this.translate = translate;
+        this.rotate = rotate;
+        this.scale = scale;
         this.onPick = onPick;
     }
 
@@ -15,5 +20,27 @@ export class MyButton extends Pickable {
         this.registerPickable(this.scene, this);
         this.button.display();
         this.unregisterPickable(this.scene);
+        
+        this.displayText();
+    }
+
+    displayText() {
+        if (this.text) {
+            this.scene.pushMatrix();
+            const prevShader = this.scene.activeShader;
+            // activate shader for rendering text characters
+            this.scene.setActiveShaderSimple(this.scene.textShader);
+            // activate texture containing the font
+            this.scene.textAppearance.apply();
+    
+            // 	Reset transf. matrix to draw independent of camera
+            this.scene.loadIdentity();
+    
+            // transform as needed to place on screen
+            this.scene.translate(this.translate[0], this.translate[1], this.translate[2]);
+            writeText(this.scene, this.text);
+            this.scene.setActiveShader(prevShader);
+            this.scene.popMatrix();
+        }
     }
 }
